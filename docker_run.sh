cc4bd486b258d6aa055d645bee0f92c308fc1f41
Mateus Gasparino <matvalverde@gmail.com>
2023-06-15 17:17:31 -0500

    added docker_run.sh script

---
+++ docker_run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# run the container
+sudo docker run --runtime nvidia -it --rm \
+    --network host \
+    --device /dev/video0 \
+    --device /dev/video1 \
+	matvalverde/unitree_cam:latest
