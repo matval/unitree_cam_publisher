39e676f5515677e0d0f75d01d655151d1e32f4b5
Mateus V Gasparino <matvalverde@gmail.com>
2023-06-26 14:37:04 -0500

    improving scrips

--- docker_run.sh
+++ docker_run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 # run the container
-docker run --runtime nvidia -it --rm \
+docker run --runtime nvidia --rm \
     --network host \
     --device /dev/video0 \
     --device /dev/video1 \
-	matvalverde/unitree_cam:latest
+	matvalverde/unitree_cam:latest \
+    ros2 run unitree_cameras camera_node 0 \
+    ros2 run unitree_cameras camera_node 1
--- unitree_cam_publisher/src/camera_node.cpp
+++ unitree_cam_publisher/src/camera_node.cpp
@@ -53,6 +53,16 @@ int main(int argc, char **argv)
         if(argc >=5)
             fps = std::atoi(argv[4]);
     }
+
+    std::string topic_name;
+    if(deviceNode == 0)
+    {
+        topic_name = "left_stereo";
+    }
+    else if(deviceNode == 1)
+    {
+        topic_name = "right_stereo";
+    }
     
     // Load camera stuf
     UnitreeCamera cam(deviceNode); //"stereo_camera_config.yaml"); ///< init camera by device node number
@@ -64,7 +74,7 @@ int main(int argc, char **argv)
     cam.setRectFrameSize(cv::Size(frameSize.width >> 2, frameSize.height >> 1)); ///< set camera rectify frame size
     
     cam.startCapture(); ///< disable image h264 encoding and share memory sharing
-    cam.startStereoCompute(); ///< start disparity computing
+    // cam.startStereoCompute(); ///< start disparity computing
     
     usleep(500000);
 
@@ -74,98 +84,157 @@ int main(int argc, char **argv)
     rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("image_publisher", options);
     image_transport::ImageTransport it(node);
     // image_transport::Publisher color_pub = it.advertise("camera/image", 1);
-    image_transport::CameraPublisher cam_pub = it.advertiseCamera("camera/rect_image", 1);
-    image_transport::Publisher depth_pub = it.advertise("camera/depth", 1);
+    image_transport::CameraPublisher left_cam_pub = it.advertiseCamera(topic_name + "/left_camera/rect_image", 1);
+    image_transport::CameraPublisher right_cam_pub = it.advertiseCamera(topic_name + "/right_camera/rect_image", 1);
+    // image_transport::Publisher depth_pub = it.advertise("camera/depth", 1);
 
     std_msgs::msg::Header image_header;
-    sensor_msgs::msg::Image::SharedPtr color_msg, depth_msg;
-    sensor_msgs::msg::CameraInfo::SharedPtr cam_info(new sensor_msgs::msg::CameraInfo());
+    sensor_msgs::msg::Image::SharedPtr left_msg, right_msg; //, depth_msg;
+    sensor_msgs::msg::CameraInfo::SharedPtr left_cam_info(new sensor_msgs::msg::CameraInfo());
+    sensor_msgs::msg::CameraInfo::SharedPtr right_cam_info(new sensor_msgs::msg::CameraInfo());
 
-    // Get calibration parameters
-    std::vector<cv::Mat> paramsArray;
-    if(cam.getCalibParams(paramsArray))
+    // Get calibration parameters for the right camera
+    std::vector<cv::Mat> right_params;
+    if(cam.getCalibParams(right_params, true))
     {
         // intrinsic, distortion, xi, rotation, translation, kfe
-        for(std::size_t i=0; i<paramsArray.size(); i++)
+        for(std::size_t i=0; i<right_params.size(); i++)
         {
-            std::cout << "\ndata:\n" << paramsArray[i] << std::endl;
+            std::cout << "\ndata:\n" << right_params[i] << std::endl;
         }
 
         // Populate CameraInfo message
-        cam_info->distortion_model = "rational_polynomial";
-        for(int i=0; i<paramsArray[0].rows*paramsArray[0].cols; i++)
+        right_cam_info->distortion_model = "rational_polynomial";
+        for(int i=0; i<right_params[0].rows*right_params[0].cols; i++)
         {
-            cam_info->k[i] = paramsArray[0].reshape(1).at<double>(i);
+            right_cam_info->k[i] = right_params[0].reshape(1).at<double>(i);
         }
-        cam_info->d.resize(4, 0.0);
-        for(int i=0; i<paramsArray[1].rows*paramsArray[1].cols; i++)
+        right_cam_info->d.resize(4, 0.0);
+        for(int i=0; i<right_params[1].rows*right_params[1].cols; i++)
         {
-            cam_info->d[i] = paramsArray[1].reshape(1).at<double>(i);
+            right_cam_info->d[i] = right_params[1].reshape(1).at<double>(i);
         }
-        for(int i=0; i<paramsArray[3].rows*paramsArray[3].cols; i++)
+        for(int i=0; i<right_params[3].rows*right_params[3].cols; i++)
         {
-            cam_info->r[i] = paramsArray[3].reshape(1).at<double>(i);
+            right_cam_info->r[i] = right_params[3].reshape(1).at<double>(i);
         }
-        for(int i=0; i<paramsArray[5].rows*paramsArray[5].cols; i++)
+        for(int i=0; i<right_params[5].rows*right_params[5].cols; i++)
         {
-            cam_info->p[i] = paramsArray[5].reshape(1).at<double>(i);
+            right_cam_info->p[i] = right_params[5].reshape(1).at<double>(i);
+        }
+    }
+
+    // Get calibration parameters for the left camera
+    std::vector<cv::Mat> left_params;
+    if(cam.getCalibParams(left_params, false))
+    {
+        // intrinsic, distortion, xi, rotation, translation, kfe
+        for(std::size_t i=0; i<left_params.size(); i++)
+        {
+            std::cout << "\ndata:\n" << left_params[i] << std::endl;
+        }
+
+        // Populate CameraInfo message
+        left_cam_info->distortion_model = "rational_polynomial";
+        for(int i=0; i<left_params[0].rows*left_params[0].cols; i++)
+        {
+            left_cam_info->k[i] = left_params[0].reshape(1).at<double>(i);
+        }
+        left_cam_info->d.resize(4, 0.0);
+        for(int i=0; i<left_params[1].rows*left_params[1].cols; i++)
+        {
+            left_cam_info->d[i] = left_params[1].reshape(1).at<double>(i);
+        }
+        for(int i=0; i<left_params[3].rows*left_params[3].cols; i++)
+        {
+            left_cam_info->r[i] = left_params[3].reshape(1).at<double>(i);
+        }
+        for(int i=0; i<left_params[5].rows*left_params[5].cols; i++)
+        {
+            left_cam_info->p[i] = left_params[5].reshape(1).at<double>(i);
         }
     }
     
     rclcpp::WallRate loop_rate(30);
     while(rclcpp::ok() && cam.isOpened())
     {
-        cv::Mat left, right, depth;
+        cv::Mat left, right; //, depth; //, dispf;
         std::chrono::microseconds t;
 
-        if(!cam.getRectStereoFrame(left, right)){ ///< get rectify left,right frame  
-            usleep(1000);
+        // Get rectified left andright frames  
+        if(!cam.getRectStereoFrame(left, right))
+        {
+            usleep(50);
             continue;
         }
 
-        // Try with disparity
-        getDepthFrame(cv::Mat& dispf, cv::Mat& depth);
-        if(!cam.getDepthFrame(depth, false, t)){  ///< get stereo camera depth image 
-            usleep(1000);
-            continue;
-        }
+        // Get stereo camera depth image
+        // cam.getDepthFrame(dispf, depth);
+
+        // if(!cam.getDepthFrame(depth, false, t))
+        // {
+        //     usleep(50);
+        //     continue;
+        // }
 
         image_header.stamp = node->get_clock()->now();;
-        image_header.frame_id = "camera_frame";
 
         if (!left.empty())
         {
+            image_header.frame_id = "left_camera_frame";
             // Debug color image type
             // std::string ty =  type2str( left.type() );
             // printf("Color: %s %dx%d \n", ty.c_str(), left.cols, left.rows);
 
-            color_msg = cv_bridge::CvImage(image_header, "bgr8", left).toImageMsg();
+            cv::flip(left, left, -1);
+            left_msg = cv_bridge::CvImage(image_header, "bgr8", left).toImageMsg();
 
-            cam_info->height = left.rows;
-            cam_info->width = left.cols;
-            cam_info->header = image_header;
+            left_cam_info->height = left.rows;
+            left_cam_info->width = left.cols;
+            left_cam_info->header = image_header;
 
             // Publish via image_transport
-            cam_pub.publish(color_msg, cam_info);
-            cv::waitKey(1);
+            left_cam_pub.publish(left_msg, left_cam_info);
+            // cv::waitKey(1);
         }
 
-        if (!depth.empty())
+        if (!right.empty())
         {
-            // Debug depth image type
-            // std::string ty =  type2str( depth.type() );
-            // printf("Depth: %s %dx%d \n", ty.c_str(), depth.cols, depth.rows);
+            image_header.frame_id = "right_camera_frame";
+            // Debug color image type
+            // std::string ty =  type2str( left.type() );
+            // printf("Color: %s %dx%d \n", ty.c_str(), left.cols, left.rows);
+            cv::flip(right, right, -1);
+            right_msg = cv_bridge::CvImage(image_header, "bgr8", right).toImageMsg();
 
-            depth_msg = cv_bridge::CvImage(image_header, "bgr8", depth).toImageMsg();
-            depth_pub.publish(depth_msg);
-            cv::waitKey(1);
+            right_cam_info->height = right.rows;
+            right_cam_info->width = right.cols;
+            right_cam_info->header = image_header;
+
+            // Publish via image_transport
+            right_cam_pub.publish(right_msg, right_cam_info);
+            // cv::waitKey(1);
         }
+
+        // if (!depth.empty())
+        // {
+        //     // Debug depth image type
+        //     // std::string ty =  type2str( depth.type() );
+        //     // printf("Depth: %s %dx%d \n", ty.c_str(), depth.cols, depth.rows);
+
+        //     // std::string ty2 =  type2str( dispf.type() );
+        //     // printf("Disparity: %s %dx%d \n", ty2.c_str(), dispf.cols, dispf.rows);
+
+        //     depth_msg = cv_bridge::CvImage(image_header, "bgr8", depth).toImageMsg();
+        //     depth_pub.publish(depth_msg);
+        //     // cv::waitKey(1);
+        // }
 
         rclcpp::spin_some(node);
         loop_rate.sleep();
     }
     
-    am.stopStereoCompute();
+    // cam.stopStereoCompute();
     cam.stopCapture(); ///< stop camera capturing
 
     return 0;
